59068c861cad3f724160ee04ec7eb3506d52754d
Maciej Gasiorek <maciej.gasiorek@pb.com>
2024-01-29 23:16:19 +0100

    homework

--- cypress/e2e/justForTest/test_part_1.spec.ts
+++ cypress/e2e/justForTest/test_part_1.spec.ts
@@ -1,39 +1,52 @@
+import { use } from 'chai'
 import { baseData } from 'cypress/fixtures/base.json'
 import { controllers } from 'cypress/fixtures/controllers.json'
 import { Helpers } from 'cypress/support/helpers'
+import {user} from 'cypress/support/registration.data.json'
 
 describe('Register / login / logout users', () => {
     const helpers = new Helpers();
     const testUserName = `TestName_${Math.random().toString().slice(2,12)}` 
     const testUserEmail= `TestName_${Math.random().toString().slice(2,12)}@op.pl` 
+    const testUserPassword= `TestName_${Math.random().toString().slice(2,12)}` 
 
     beforeEach(() => {
         // runs once before all tests
     })
 
-    // -- Start: Cypress Tests --
+
     it('Register user test', () => {
         
-
         cy.visit(baseData.defaultUrl)
         helpers.getElement(controllers.static.logo).should('be.visible');
-        //cy.xpath('//img[contains(@src,\'logo.png\')]').should('be.visible');
         helpers.getElement(controllers.button.loginLink).click();
         helpers.getElement(controllers.text.newUserSignUp).should('be.visible');
-
         helpers.getElement(controllers.input.name).type(testUserName);
         helpers.getElement(controllers.input.email).type(testUserEmail);
         helpers.getElement(controllers.button.signUp).click();
 
+        helpers.getElement(controllers.input.title).contains(user.title).click();
+        helpers.getElement(controllers.input.password).type(testUserPassword);
+        helpers.getElement(controllers.select.day).select(user.day);
+        helpers.getElement(controllers.select.months).select(user.month);
+        helpers.getElement(controllers.select.year).select(user.year);
+        helpers.getElement(controllers.input.newsletter).click();
+        helpers.getElement(controllers.input.firstName).type(user.firstName);
+        helpers.getElement(controllers.input.lastName).type(user.lastName);
+        helpers.getElement(controllers.input.company).type(user.company);
+        helpers.getElement(controllers.input.address1).type(user.address1);
+        helpers.getElement(controllers.select.country).select(user.country);
+        helpers.getElement(controllers.input.state).type(user.state);
+        helpers.getElement(controllers.input.city).type(user.city);
+        helpers.getElement(controllers.input.zipcode).type(user.zipcode);
+        helpers.getElement(controllers.input.mobileNumber).type(user.mobileNumber);
+        helpers.getElement(controllers.button.createAccount).click();
+        helpers.getElement(controllers.text.accountCreated).should('be.visible');
+        helpers.getElement(controllers.button.Continue).click();
+        helpers.getElement(controllers.text.loggedInAs).should('be.visible');
         
 
-        // const cssSelector: string = '#myElement';
-        // const xpathSelector: string = '//div[@class="example"]';
-        // const unknownSelector: string = 'someSelector';
-
-        // cy.log(`Type of "${cssSelector}": ${helpers.getElement(cssSelector)}`)
-        // cy.log(`Type of "${xpathSelector}": ${helpers.getElement(xpathSelector)}`)
-        //cy.log(`Type of "${unknownSelector}": ${helpers.getElement(unknownSelector)}`)
+     
     })
 
 
